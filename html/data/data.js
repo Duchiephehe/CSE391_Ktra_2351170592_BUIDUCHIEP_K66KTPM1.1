928770ed82953dcffc1be8d3d77d92732e77edfc
Bùi Đức Hiệp <hieptop12@gmail.com>
2026-06-10 07:56:59 +0700

    git commit -m "feat(html): tạo dữ liệu  5 sản phẩm vào file data.js" (Nội dung: Hoàn thiện file html/data/data.js).

---
+++ html/data/data.js
@@ -0,0 +1,47 @@
+(function (window) {
+    const PRODUCTS = [
+        {
+            id: 1,
+            name: "Áo thun Basic",
+            price: 150000,
+            quantity: 25,
+            description: "Áo cotton thoáng mát dạng unisex",
+            image: "assets/img/product1.jpg"
+        },
+        {
+            id: 2,
+            name: "Quần Jeans",
+            price: 420000,
+            quantity: 12,
+            description: "Denim co giãn, nhiều size",
+            image: "assets/img/product2.jpg"
+        },
+        {
+            id: 3,
+            name: "Giày Thể Thao",
+            price: 860000,
+            quantity: 8,
+            description: "Đế êm, thiết kế hiện đại",
+            image: "assets/img/product3.jpg"
+        },
+        {
+            id: 4,
+            name: "Áo Khoác Gió",
+            price: 320000,
+            quantity: 18,
+            description: "Chống nắng, nhẹ và gọn",
+            image: "assets/img/product4.jpg"
+        },
+        {
+            id: 5,
+            name: "Túi Vải Tote",
+            price: 120000,
+            quantity: 40,
+            description: "Túi vải thân thiện môi trường",
+            image: "assets/img/product5.jpg"
+        }
+    ];
+
+    window.PRODUCTS = PRODUCTS;
+})(window);
+
